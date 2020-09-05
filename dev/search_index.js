495b77f5305e2ea3a9aaaec29116658d376bc595
Documenter.jl <documenter@juliadocs.github.io>
2020-09-05 12:45:49 +0000

    build based on 7b75553

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NDVoronoi","category":"page"},{"location":"#NDVoronoi","page":"Home","title":"NDVoronoi","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NDVoronoi]","category":"page"}]
+}
